61c38083aca2f2f45038130a760d6f3006507fc7
Vaibhav Garg <vaibhavgarg2003.vg@gmail.com>
2023-11-29 01:56:28 +0530

    Update note - title change

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -57,7 +57,7 @@ const Notes = (props) => {
                 <div className="modal-dialog">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                            <h1 className="modal-title fs-5" id="staticBackdropLabel">Update Note</h1>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
