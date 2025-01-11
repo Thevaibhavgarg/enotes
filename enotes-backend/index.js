6a7a4509204551930620f21fa7cb59e8b6e52493
Vaibhav Garg <vaibhavgarg2003.vg@gmail.com>
2025-01-11 23:25:05 +0530

    adding auth token in cors

--- enotes-backend/index.js
+++ enotes-backend/index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization","auth-token");
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
   }
