4b187a6040a85b48bb2c548d11466e04fcb867e6
Vaibhav Garg <vaibhavgarg2003.vg@gmail.com>
2025-01-11 19:40:27 +0530

    cors update

--- enotes-backend/index.js
+++ enotes-backend/index.js
@@ -8,25 +8,32 @@ connectToMongo();
 const app = express()
 const port = process.env.PORT || 8000
 
-const corsConfig = {
-  origin: 'https://enotes-sigma.vercel.app',
-  methods: ["GET","POST","DELETE","PUT"],
-  credentials: true
-}
-app.use(cors(corsConfig));
-app.options("*",cors(corsConfig));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
+app.use(cors({
+  origin: 'https://enotes-sigma.vercel.app', // Allow only this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  credentials: true // If you need to send cookies or authentication credentials
+}));
+
+// const corsConfig = {
+//   origin: 'https://enotes-sigma.vercel.app',
+//   methods: ["GET","POST","DELETE","PUT"],
+//   credentials: true
+// }
+// app.use(cors(corsConfig));
+// app.options("*",cors(corsConfig));
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(200);
+//   }
+//   next();
+// });
 // app.use(cors());
 app.use(express.json());
 
